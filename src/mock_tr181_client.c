88abc29d8631afb0440e7372bd0612d009a38dcb
Murugan Viswanathan <muruganviswanathan@gmail.com>
2018-03-01 00:00:46 -0800

    Added cmd line options for parodus port, client port and  db file (#4)
    * Initial version of mock_tr181
    
    * Initial version of mock_tr181
    
    * Added dependencies and includes
    
    * code for mock_client
    
    * code for mock_client
    
    * code for mock_client
    
    * code for mock_client
    
    * First Working version of mock tr181 client
    
    * Fixed copyright year, removed company reference
    
    * Fixed copyright year, removed company reference
    
    * coded - untested for wildcard
    
    * coded - untested for wildcard
    
    * coded - untested for wildcard
    
    * coded - untested for wildcard
    
    * coded - untested for wildcard
    
    * coded - untested for wildcard
    
    * coded - untested for wildcard
    
    * changed mock data
    
    * Wildcard GET implementation
    
    * Added command line options
    
    * Added command line options
    
    * Added command line options
    
    * Added command line options
    
    * Added command line options

--- src/mock_tr181_client.c
+++ src/mock_tr181_client.c
@@ -15,8 +15,6 @@
  *
  */
 
-//#include <stdbool.h>
-//#include <stdint.h>
 
 #include "mock_tr181_client.h"
 #include "mock_tr181_adapter.h"
@@ -25,8 +23,13 @@
 /*                                   Macros                                   */
 /*----------------------------------------------------------------------------*/
 
+#define P2M_PARODUS_PORT_NUM 6666
+
+#define P2M_MOCK_PORT_NUM 6663
+
 // Limit for total number of wild card matched params in the Response
-#define MAX_WILDCARD_PARAMS 100
+#define P2M_MAX_WILDCARD_PARAMS 100
+
 
 /*----------------------------------------------------------------------------*/
 /*                            File Scoped Variables                           */
@@ -64,15 +67,18 @@ const char *rdk_logger_module_fetch(void)
 	return "LOG.RDK.MOCK_TR181";
 }
 
-void connect_parodus()
+void connect_parodus_default()
 {
+	Info("Using default values for Parodus PORT and Client Port.\n");
 
-	libpd_cfg_t cfg = { .service_name = "config", .receive = true,
-			.keepalive_timeout_secs = 64, .parodus_url = "tcp://127.0.0.1:6666",
+	libpd_cfg_t cfg = {
+			.service_name = "config",
+			.receive = true,
+			.keepalive_timeout_secs = 64,
+			.parodus_url = "tcp://127.0.0.1:6666",
 			.client_url = "tcp://127.0.0.1:6663" };
 
-	Info(
-			"Configurations => service_name : %s parodus_url : %s client_url : %s\n",
+	Info("Configurations => service_name : %s parodus_url : %s client_url : %s\n",
 			cfg.service_name, cfg.parodus_url, cfg.client_url);
 
 	while (1)
@@ -93,6 +99,76 @@ void connect_parodus()
 
 	}
 }
+
+void connect_parodus(char* parodus_port, char* client_port)
+{
+	if(parodus_port == NULL &&  client_port == NULL)
+	{
+		return connect_parodus_default();
+	}
+
+	char str_parodus_url[128] = {};
+	char str_client_url[128] = {};
+	memset(str_parodus_url, 0, 128);
+	memset(str_client_url, 0, 128);
+
+	if(parodus_port)
+	{
+		snprintf(str_parodus_url, 128-1, "tcp://127.0.0.1:%s", parodus_port);
+	}
+	else
+	{
+		Info("Using default value for Parodus PORT.\n");
+		strncpy(str_parodus_url, "tcp://127.0.0.1:6666", strlen("tcp://127.0.0.1:6666")+1);
+	}
+
+	if(client_port)
+	{
+		snprintf(str_client_url, 128-1, "tcp://127.0.0.1:%s", client_port);
+	}
+	else
+	{
+		Info("Using default value for Client PORT.\n");
+		strncpy(str_client_url, "tcp://127.0.0.1:6663", strlen("tcp://127.0.0.1:6663")+1);
+	}
+
+	//libpd_cfg_t cfg = { .service_name = "config", .receive = true,
+	//		.keepalive_timeout_secs = 64, .parodus_url = "tcp://127.0.0.1:6666",
+	//		.client_url = "tcp://127.0.0.1:6663" };
+
+	libpd_cfg_t cfg = {
+			.service_name = "config",
+			.receive = true,
+			.keepalive_timeout_secs = 64,
+			.parodus_url = str_parodus_url,
+			.client_url = str_client_url
+		};
+
+	Info( "Configurations => service_name : \"%s\", parodus_url : \"%s\", client_url : \"%s\"\n",
+			cfg.service_name,
+			cfg.parodus_url,
+			cfg.client_url );
+
+	while (1)
+	{
+		int ret = libparodus_init(&mock_tr181_instance, &cfg);
+
+		if (ret == 0)
+		{
+			Info("Init for parodus Success..!!\n");
+			break;
+		}
+		else
+		{
+			Error("Init for parodus failed ! : '%s'\n", libparodus_strerror(ret));
+			sleep(5);
+		}
+
+		libparodus_shutdown(&mock_tr181_instance);
+
+	}
+}
+
 
 void startParodusReceiveThread()
 {
@@ -149,7 +225,9 @@ static void processRequest(char *reqPayload, char **resPayload)
 	 * get all the TR181 params from db file.
 	 * Note: db in json format
 	 */
-	int status = readFromDB(&dbData);
+  
+	int status = mock_tr181_db_read(&dbData);
+
 	if (status == 1)
 	{
 		Info("Data from DB: %s\n", dbData);
@@ -522,7 +600,9 @@ static void processRequest(char *reqPayload, char **resPayload)
 
 			addData = cJSON_Print(paramList);
 			Print("addData : %s\n", addData);
-			status = writeToDB(addData);
+      
+			status = mock_tr181_db_write(addData);
+
 			if (status == 1)
 			{
 				Info("Data is successfully added to DB\n");
--- src/mock_tr181_main.c
+++ src/mock_tr181_main.c
@@ -15,11 +15,68 @@
  *
  */
 
+
+#include <getopt.h>
+#include "mock_tr181_adapter.h"
 #include "mock_tr181_client.h"
 
-int main()
+int main( int argc, char **argv)
 {
-	connect_parodus();
+    const char *option_string = "p:c:d:h::";
+    static const struct option options[] = {
+        { "help",          optional_argument, 0, 'h' },
+        { "parodus-port",  optional_argument, 0, 'p' },
+        { "client-port",   optional_argument, 0, 'c' },
+        { "db-file",       optional_argument, 0, 'd' },
+        { 0, 0, 0, 0 }
+    };
+
+    int item = 0, opt_index = 0;
+    char* parodus_port = NULL;
+    char* client_port = NULL;
+
+    while( -1 != (item = getopt_long(argc, argv, option_string, options, &opt_index)) )
+	{
+		switch (item)
+		{
+			case 'p':
+				Info("Option p read\n");
+				parodus_port = strdup(optarg);
+				break;
+			case 'c':
+				Info("Option c read\n");
+				client_port = strdup(optarg);
+				break;
+			case 'd':
+				Info("Option d read\n");
+				mock_tr181_db_init(optarg);
+				break;
+			case 'h':
+				Info("Option h read\n");
+				printf("Usage:%s [-p <parodus_port>] [-c <client_port>] [-d <fully_qualified_database_name>]\n", argv[0]);
+				return 0;
+			case '?':
+				Info("Option invalid\n");
+				if (strchr(option_string, optopt))
+				{
+					printf("%s Option %c requires an argument!\n", argv[0], optopt);
+					printf("Usage:%s [-p <parodus_port>] [-c <client_port>] [-d <fully_qualified_database_name>]\n", argv[0]);
+					break;
+				}
+				else
+				{
+					printf("%s Unrecognized option %c\n", argv[0], optopt);
+				}
+
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	connect_parodus(parodus_port, client_port);
+
 	startParodusReceiveThread();
 	sleep(5);
 
